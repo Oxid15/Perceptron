73b65243b53030a3e0f5f1a2d0fb4e9391cb709d
Ilia Moiseev <villeman.5@yandex.ru>
2021-09-11 19:23:35 +0300

    New main function with simple usage

--- Perceptron/Main.cpp
+++ Perceptron/Main.cpp
@@ -1,30 +1,14 @@
-//TODO change name of all "size" function parameters to more appropriate ones
-
 #include<iostream>
-#include"Evolution.cpp"
+#include"NeuralNet.cpp"
+
+
 int main()
 {
-	std::fstream file("test.csv");
-	double** arr = readCsv<double>(file, 78, 1);
-	double* a = new double[78];
-	for (int i = 0; i < 78; i++)
-		a[i] = arr[i][0];
-	
-	std::cout << "              mean             = " << mean(a, 78) << "\n";
-	std::cout << "              median           = " << median(a, 78) << "\n";
-	std::cout << "[shifted]     variance         = " << variance(a, 78, true) << "\n";
-	std::cout << "[not_shifted] variance         = " << variance(a, 78, false) << "\n";
-	std::cout << "[shifted]     SD               = " << SD(a, 78, true) << "\n";
-	std::cout << "[not_shifted] SD               = " << SD(a, 78, false) << "\n";
-	std::cout << "              measure of scale = " << MeasOfScale(a, 78) << "\n";
-	std::cout << "\n";
-	std::cout << "[shifted][raw]             1-th moment = " << rawKthMoment(a, 1, 78) << "\n";
-	std::cout << "[shifted][raw]             2-th moment = " << rawKthMoment(a, 2, 78) << "\n";
-	std::cout << "[shifted][raw]             3-th moment = " << rawKthMoment(a, 3, 78) << "\n";
-	std::cout << "[shifted][raw]             4-th moment = " << rawKthMoment(a, 4, 78) << "\n";
-	std::cout << "\n";
-	std::cout << "[shifted][central]         1-th moment = " << centralKthMoment(a, 1, 78) << "\n";
-	std::cout << "[shifted][central]         2-th moment = " << centralKthMoment(a, 2, 78) << "\n";
-	std::cout << "[shifted][central]         3-th moment = " << centralKthMoment(a, 3, 78) << "\n";
-	std::cout << "[shifted][central]         4-th moment = " << centralKthMoment(a, 4, 78) << "\n";
+	NeuralNet<double> net("testConfig.nn");
+	double input[] = {1., 1.};
+	double* output = new double;
+
+	output = net.process(input);
+	std::cout << output[0] << std::endl;
+	std::system("pause");
 }
